e106576d8ceed9803d431e0d3d4a2a17c6b8f56a
EricSampaio <erick.gtn.xp@gmail.com>
2023-10-18 16:17:32 -0300

    Adicionando o arquivo menu

---
+++ menu.py
@@ -0,0 +1,7 @@
+def menu():
+    print("################ CADASTRO DE CLIENTES ################")
+    print("O que deseja fazer:")
+    print(" 1 - Cadastrar Cliente: \n 2 - Exibir Clientes: \n 3 - Consultar Cliente pelo CPF: \n 4 - Alterar Dados Cadastrais: \n 5 - Excluir Cliente: \n 6 - Sair")
+    global opcao
+    opcao = input()
+    return opcao
